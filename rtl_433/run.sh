ff1003cda0ec6b6362326e629706a9859249b60c
catduckgnaf <github@catduck.org>
2023-10-03 10:20:49 -0400

    Update run.sh

--- rtl_433/run.sh
+++ rtl_433/run.sh
@@ -3,15 +3,55 @@
 conf_directory="/config/rtl_433"
 conf_file="rtl_433.conf"
 
-if bashio::services.available "mqtt"; then
-    host=$(bashio::services "mqtt" "host")
-    password=$(bashio::services "mqtt" "password")
-    port=$(bashio::services "mqtt" "port")
-    username=$(bashio::services "mqtt" "username")
-    retain=$(bashio::config "retain")
+if [ ! -z ${MQTT_HOST+x} ]; then
+  echo "Running in stand-alone docker mode"
+  MQTT_PORT="${MQTT_PORT:-1883}"
+  RTL_TOPIC="${RTL_TOPIC:-rtl_433/+/events}"
+  DISCOVERY_PREFIX="${DISCOVERY_PREFIX:-homeassistant}"
+  DISCOVERY_INTERVAL="${DISCOVERY_INTERVAL:-600}"
+  OTHER_ARGS="${OTHER_ARGS-}"
+
+  LOG_LEVEL="${LOG_LEVEL-}"
+  if [[ $LOG_LEVEL == "quiet" ]]; then
+    OTHER_ARGS="${OTHER_ARGS} --quiet"
+  fi
+  if [[ $LOG_LEVEL == "debug" ]]; then
+    OTHER_ARGS="${OTHER_ARGS} --debug"
+  fi
 else
-    bashio::log.info "The mqtt addon is not available."
-    bashio::log.info "Manually update the output line in the configuration file with mqtt connection settings, and restart the addon."
+  if bashio::services.available mqtt; then
+    echo "mqtt found in this Home Assistance instance."
+    MQTT_HOST=$(bashio::services mqtt "host")
+    MQTT_PORT=$(bashio::services mqtt "port")
+    export MQTT_USERNAME=$(bashio::services mqtt "username")
+    export MQTT_PASSWORD=$(bashio::services mqtt "password")
+  else
+    echo "Using an external mqtt broker."
+    MQTT_HOST=$(bashio::config "mqtt_host")
+    MQTT_PORT=$(bashio::config "mqtt_port")
+    export MQTT_USERNAME=$(bashio::config "mqtt_user")
+    export MQTT_PASSWORD=$(bashio::config "mqtt_password")
+  fi
+
+  RTL_TOPIC=$(bashio::config "rtl_topic")
+  DISCOVERY_PREFIX=$(bashio::config "discovery_prefix")
+  DISCOVERY_INTERVAL=$(bashio::config "discovery_interval")
+
+  OTHER_ARGS=""
+  if bashio::config.true "mqtt_retain"; then
+    OTHER_ARGS="${OTHER_ARGS} --retain"
+  fi
+  if bashio::config.true "force_update"; then
+    OTHER_ARGS="${OTHER_ARGS} --force_update"
+  fi
+
+  LOG_LEVEL=$(bashio::config "log_level")
+  if [[ $LOG_LEVEL == "quiet" ]]; then
+    OTHER_ARGS="${OTHER_ARGS} --quiet"
+  fi
+  if [[ $LOG_LEVEL == "debug" ]]; then
+    OTHER_ARGS="${OTHER_ARGS} --debug"
+  fi
 fi
 
 if [ ! -d $conf_directory ]
@@ -23,8 +63,7 @@ fi
     rtl_433 -c "/config/rtl_433/$conf_file" -F log
 
     tag=$(basename "$conf_file")
-    rtl_433 -c "$conf_file" > >(sed -u "s/^/[$tag] /") 2> >(>&2 sed -u "s/^/[$tag] /")&
-    rtl_433_pids+=($!)
+    rtl_433 -c "$conf_file" -H $MQTT_HOST -p $MQTT_PORT -R "$RTL_TOPIC" -D "$DISCOVERY_PREFIX" -i $DISCOVERY_INTERVAL $OTHER_ARGS
 done
 
 wait -n ${rtl_433_pids[*]}
