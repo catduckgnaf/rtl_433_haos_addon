8808075c6b2b6a4cf25fc8347a592963646a7e9d
cellardoor452 <github@catduck.org>
2023-10-29 21:21:17 -0400

    builds

---
+++ rtl_433-next/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/with-contenv bashio
+
+conf_directory="/config/rtl_433"
+conf_file="rtl_433.conf"
+
+if [ ! -d "$conf_directory" ]; then
+    mkdir -p "$conf_directory"
+fi
+
+if [ ! -f "$conf_directory/$conf_file" ]; then
+    wget -P "$conf_directory" "https://raw.githubusercontent.com/catduckgnaf/rtl_433_ha/main/config/rtl_433.conf"
+fi
+
+if output_options=$(bashio::config "websocket"); then
+    host=$(bashio::config "ws_http_host")
+    port=$(bashio::config "ws_http_port")
+    echo "Starting rtl_433 with Websocket Option $conf_file..."
+    rtl_433 -c "/config/rtl_433/$conf_file" -F "http://$host:$port"
+else
+    if output_options=$(bashio::config "mqtt"); then
+        host=$(bashio::services "mqtt" "host")
+        password=$(bashio::services "mqtt" "password")
+        port=$(bashio::services "mqtt" "port")
+        username=$(bashio::services "mqtt" "username")
+        retain=$(bashio::config "retain")
+        echo "Starting rtl_433 with MQTT Option $conf_file..."
+        rtl_433 -c "/config/rtl_433/$conf_file" -F "mqtt://$host:$port,retain=0,devices=rtl_433[/id]"
+    fi
+fi
+
+wait -n "${rtl_433_pids[*]}"
