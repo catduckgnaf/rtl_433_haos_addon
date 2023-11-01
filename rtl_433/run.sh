658c9ea83fa067e59937c305e8f946dc7da30f3f
catduckdngaf <git@catduck.org>
2023-11-01 14:23:58 -0400

    fixing what i broke

--- rtl_433/run.sh
+++ rtl_433/run.sh
@@ -3,15 +3,8 @@
 
 conf_directory="/config/rtl_433"
 conf_file="rtl_433.conf"
-rtl_433_pids=() # Initialize an array to store process IDs
 
-# Function to handle errors and exit the script
-handle_error() {
-    local exit_code=$1
-    local error_message=$2
-    echo "Error: $error_message" >&2
-    exit "$exit_code"
-}
+
 
 # Check if the configuration directory exists
 if [ ! -d "$conf_directory" ]; then
@@ -28,8 +21,7 @@ if output_options=$(bashio::config "websocket"); then
     host=$(bashio::config "ws_http_host")
     port=$(bashio::config "ws_http_port")
     echo "Starting rtl_433 with websocket option on $host:$port with $conf_file..."
-    rtl_433 -c "$conf_directory/$conf_file" -F "http://$host:$port" &
-    rtl_433_pids+=($!)
+    rtl_433 -c "$conf_directory/$conf_file" -F "http://$host:$port"
 
 elif output_options=$(bashio::config "mqtt"); then
     host=$(bashio::config "mqtt_host")
@@ -39,12 +31,6 @@ elif output_options=$(bashio::config "mqtt"); then
     retain=$(bashio::config "retain")
     echo "Starting rtl_433 with MQTT Option $conf_file..."
     rtl_433 -c "$conf_directory/$conf_file" -F "mqtt://$host:$port,retain=1,devices=rtl_433[/id]" &
-    rtl_433_pids+=($!)
 else
     handle_error 3 "No valid output options specified in the configuration"
 fi
-
-# Wait for all background rtl_433 processes to complete
-for pid in "${rtl_433_pids[@]}"; do
-    wait "$pid" || handle_error 4 "One of the rtl_433 processes failed to complete"
-done
