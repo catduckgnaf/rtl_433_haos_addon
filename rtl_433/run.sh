5ab2492754272f6048b92feb4f52b2a15c2c8823
cellardoor452 <github@catduck.org>
2023-11-07 16:13:52 -0500

    fixing default logging

--- rtl_433/run.sh
+++ rtl_433/run.sh
@@ -7,11 +7,6 @@ log_directory="/config/rtl_433/logs"
 conf_file="rtl_433.conf"
 http_script="rtl_433_http_ws.py"
 mqtt_script="rtl_433_mqtt_hass.py"
-error="-v"
-warning="-vv"
-debug="-vvv"
-trace="-vvvv"
-default_logging="-vvv"
 
 ## additional commands will allow the user to specify any command line options directly from the add-on yaml config
 additional_commands=""
@@ -56,13 +51,28 @@ if [ ! -f "$script_directory/$mqtt_script" ]; then
     wget "https://raw.githubusercontent.com/catduckgnaf/rtl_433_ha/main/scripts/rtl_433_mqtt_hass.py" -O "$script_directory/$mqtt_script" || handle_error 2 "Failed to download mqtt script"
 fi
 
+# set log level
+log_level=$(bashio::config "log_level")
+
+if [ "$log_level" = "error" ]; then
+   default_logging="-v"
+elif [ "$log_level" = "warn" ]; then
+   default_logging="-vv"
+elif [ "$log_level" = "debug" ]; then
+   default_logging="-vvv"
+elif [ "$log_level" = "trace" ]; then
+   default_logging="-vvvv"
+fi
+
 # Check the output options specified in the configuration
-if output_options=$(bashio::config "websocket"); then
+output_options=$(bashio::config "output_options")
+
+if [ "$output_options" = "websocket" ]; then
     rtl_433 -c "$conf_directory/$conf_file" "$default_logging" "$additional_commands" -F "http://$http_host:$http_port" &
     rtl_433_pids+=($!)
     echo "Starting rtl_433 with websocket option on $http_host:$http_port with $conf_file... $additional_commands for rtl_433_pids+=($!)" 
 
-else output_options=$(bashio::config "mqtt"); then
+elif [ "$output_options" = "mqtt" ]; then
     host=$(bashio::config "mqtt_host")
     password=$(bashio::config "mqtt_password")
     port=$(bashio::config "mqtt_port")
@@ -71,7 +81,6 @@ else output_options=$(bashio::config "mqtt"); then
     rtl_433 -c "$conf_directory/$conf_file" "$default_logging" "$additional_commands" -F "mqtt://$mqtt_host:$mqtt_port,retain=1,devices=rtl_433[/id]" &
     rtl_433_pids+=($!)
     echo "Starting rtl_433 with MQTT Option $conf_file... $additional_commands"
-
 fi
 
 if [ ${#rtl_433_pids[@]} -eq 0 ]; then
