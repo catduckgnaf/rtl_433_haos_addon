602651a9fe4616e47af1ab37fdebda3f8cdead4f
cellardoor452 <github@catduck.org>
2023-11-07 17:04:00 -0500

    Update run.sh

--- rtl_433/run.sh
+++ rtl_433/run.sh
@@ -87,9 +87,9 @@ case "$output_options" in
         host=$(bashio::config "http_host")
         port=$(bashio::config "http_port")
         additional_commands=$(bashio::config "additional_commands")
-        rtl_433 -c "$conf_directory/$conf_file" "$default_logging" $additional_commands -F "http://$host:$port" &
+        rtl_433 -c "$conf_directory/$conf_file" "$default_logging" "$additional_commands" -F "http://$host:$port" &
         rtl_433_pids+=($!)
-        echo "Starting rtl_433 with websocket option on $http_host:$http_port using $conf_file"
+        echo "Starting rtl_433 with websocket option on $host:$port using $conf_file"
         ;;
 
     "mqtt")
@@ -99,9 +99,9 @@ case "$output_options" in
         username=$(bashio::config "mqtt_username")
         retain=$(bashio::config "mqtt_retain")
         additional_commands=$(bashio::config "additional_commands")
-        rtl_433 -c "$conf_directory/$conf_file" "$default_logging" $additional_commands -F "mqtt://$host:$port,retain=1,devices=rtl_433[/id]" &
+        rtl_433 -c "$conf_directory/$conf_file" "$default_logging" "$additional_commands" -F "mqtt://$host:$port,retain=1,devices=rtl_433[/id]" &
         rtl_433_pids+=($!)
-        echo "Starting rtl_433 with MQTT Option on $mqtt_host:$mqtt_port using $conf_file"
+        echo "Starting rtl_433 with MQTT Option on $host:$port using $conf_file"
         ;;
     
     *)
