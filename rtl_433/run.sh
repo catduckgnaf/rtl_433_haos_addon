25c1e9e554050b0f9e23938840d5a1b543374974
CatDuck <129601174+catduckgnaf@users.noreply.github.com>
2024-03-31 08:43:44 -0400

    Update to 2024.4.0
    Some major, non breaking changes. Simplified Run.sh script. Added options for custom output commands, and websocket output.
    
    Future Custom integration likely will use websockets, so planning for that. Disabled discovery for now since it wasn't working, and will be focusing on custom component verse band-aid solution. Help needed/wanted.

--- rtl_433/run.sh
+++ rtl_433/run.sh
@@ -7,41 +7,6 @@ conf_file="rtl_433.conf"
 http_script="rtl_433_http_ws.py"
 mqtt_script="rtl_433_mqtt_hass.py"
 output_logfile="output.json"
-discovery=$(bashio::config 'discovery')
-discovery_host=$(bashio::config 'discovery_host')
-discovery_port=$(bashio::config 'discovery_port')
-discovery_user=$(bashio::config 'discovery_user')
-discovery_password=$(bashio::config 'discovery_password')
-discovery_topic=$(bashio::config 'discovery_topic')
-discovery_prefix=$(bashio::config 'discovery_prefix')
-discovery_interval=$(bashio::config 'discovery_interval')
-discovery_ids=$(bashio::config 'discovery_ids')
-discovery_log_level=$(bashio::config 'discovery_log_level')
-discovery_log_file="$log_directory/discovery_script.log"
-other_args=$(bashio::config 'other_args')
-
-# other args
-if bashio::config.true "discovery_retain"; then
-    other_args+=" --retain"
-fi
-if bashio::config.true "discovery_force_update"; then
-    other_args+=" --force_update"
-fi
-# This is an optional parameter and we don't want to overwrite the defaults
-device_topic_suffix=$(bashio::config "device_topic_suffix")
-if [[ -n $device_topic_suffix ]]; then
-    other_args+=" -T '${device_topic_suffix}'"
-fi
-
-
-if [[ $discovery_log_level == "quiet" ]]; then
-    other_args+=" --quiet"
-fi
-if [[ $discovery_log_level == "debug" ]]; then
-    other_args+=" --debug"
-fi
-
-rtl_433_pids=()
 
 # Function to handle errors and log them
 handle_error() {
@@ -54,79 +19,51 @@ handle_error() {
 download_file() {
     local url=$1
     local destination=$2
-    wget -q "$url" -O "$destination"
-    if [ $? -ne 0 ]; then
-        handle_error 2 "Failed to download $destination"
+    if ! wget -q "$url" -O "$destination"; then
+        handle_error 2 "Failed to download from $url to $destination"
     fi
 }
 
-# Check if the configuration directory exists and create it if not
-if [ ! -d "$conf_directory" ]; then
-    mkdir -p "$conf_directory" || handle_error 1 "Failed to create config directory"
-fi
+# Ensure the configuration, log, and script directories exist
+mkdir -p "$conf_directory" || handle_error 1 "Failed to create config directory"
+mkdir -p "$log_directory" || handle_error 1 "Failed to create log directory"
+mkdir -p "$script_directory" || handle_error 1 "Failed to create script directory"
 
-
-# Check if the log directory exists and create it if not
-if [ ! -d "$log_directory" ]; then
-    mkdir -p "$log_directory" || handle_error 1 "Failed to create log directory"
-fi
-
-# Check if the output log file exists and rename it if it's greater than 1MB
-if [ -f "$log_directory/$output_logfile" ]; then
-    file_size=$(du -b "$log_directory/$output_logfile" | cut -f1)
-
-    if [ "$file_size" -gt 5242880 ]; then  # 5242880 bytes = 5MB
-        mv -f "$log_directory/$output_logfile" "$log_directory/$output_logfile.bak" || handle_error 1 "Failed to rename $output_logfile to $output_logfile.bak"
-
-#Now we remove the original file after renaming
-        rm -f "$log_directory/$output_logfile" || handle_error 2 "Failed to remove the original file $output_logfile"
-
-    else
-        echo "$log_directory/$output_logfile is not greater than 1MB. Skipping the renaming."
+# Manage the output log file size
+if [ -f "${log_directory}/${output_logfile}" ]; then
+    file_size=$(du -b "${log_directory}/${output_logfile}" | cut -f1)
+    if [ "$file_size" -gt 5242880 ]; then  # 5MB
+        mv -f "${log_directory}/${output_logfile}" "${log_directory}/${output_logfile}.bak" || handle_error 1 "Failed to rename $output_logfile"
+        rm -f "${log_directory}/${output_logfile}" || handle_error 2 "Failed to remove the original $output_logfile"
     fi
 fi
 
-# Download the configuration file if it doesn't exist
-if [ ! -f "$conf_directory/$conf_file" ]; then
-    download_file "https://raw.githubusercontent.com/catduckgnaf/rtl_433_ha/main/config/rtl_433_catduck_template.conf" "$conf_directory/$conf_file"
-fi
+# Download configuration and scripts if they don't exist or replace if they do
+download_file "https://raw.githubusercontent.com/catduckgnaf/rtl_433_ha/main/config/rtl_433_catduck_template.conf" "${conf_directory}/${conf_file}"
+download_file "https://raw.githubusercontent.com/catduckgnaf/rtl_433_ha/main/scripts/rtl_433_mqtt_hass.py" "${script_directory}/${mqtt_script}" && chmod +x "${script_directory}/${mqtt_script}"
+download_file "https://raw.githubusercontent.com/catduckgnaf/rtl_433_ha/main/scripts/rtl_433_http_ws.py" "${script_directory}/${http_script}" && chmod +x "${script_directory}/${http_script}"
 
-# Check if the script directory exists and create it if not
-if [ ! -d "$script_directory" ]; then
-    mkdir -p "$script_directory" || handle_error 1 "Failed to create script directory"
-fi
+# Determine the output options and start rtl_433 with the appropriate settings
+output_options=$(bashio::config "output_options")
+config_cli=$(bashio::config "additional_commands")
 
+case "$output_options" in
+    "websocket")
+        rtl_433 -c "${conf_directory}/${conf_file}" $config_cli -F "http://0.0.0.0:9443" &
+        echo "Starting rtl_433 with WebSocket option using ${conf_file}"
+        ;;
+    "mqtt")
+        rtl_433 -c "${conf_directory}/${conf_file}" $config_cli -F &
+        echo "Starting rtl_433 with MQTT option using ${conf_file}"
+        ;;
+    "custom")
+        rtl_433 -c "${conf_directory}/${conf_file}" $config_cli &
+        echo "Starting rtl_433 with custom option using ${conf_file}"
+        ;;
+    *)
+        handle_error 3 "Invalid or missing output options in the configuration"
+        ;;
+esac
 
-# Download the MQTT script if it doesn't exist and replace if it does
-if [ -f "$script_directory/$mqtt_script" ]; then
-    rm "$script_directory/$mqtt_script"
-fi
-
-download_file "https://raw.githubusercontent.com/catduckgnaf/rtl_433_ha/main/scripts/rtl_433_mqtt_hass.py" "$script_directory/$mqtt_script" && chmod +x "$script_directory/$mqtt_script"
-
-
-rtl_433 -c "$conf_directory/$conf_file" -F log
-echo "Starting rtl_433 with $conf_file located in $conf_directory"
-
-
-# Starting discovery script with logging
-
-if [ "$discovery" = true ]; then
-    echo "Starting discovery script"
-    python3 -u "$script_directory/$mqtt_script" \
-    -H $discovery_host \
-    -p $discovery_port \
-    -u "$discovery_user" \
-    -P "$discovery_password" \
-    -R "$discovery_topic" \
-    -D "$discovery_prefix" \
-    -i $discovery_interval \
-    --ids $discovery_ids 1>&1 2>&1 &
-    rtl_433_pids+=($!)
-    echo "Discovery script started with PID: ${rtl_433_pids[-1]}"
-fi
-
-# Instead of waiting for any process to finish, loop indefinitely
-while true; do
-    sleep infinity
-done
+# Keep the script running indefinitely
+sleep infinity
